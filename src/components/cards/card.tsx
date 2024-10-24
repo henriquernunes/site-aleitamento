e901d30494a3ac266d018f6ca042cc87a99ccfa8
henriquernunes <h.nunes@edu.pucrs.br>
2024-10-24 20:42:43 -0300

    fix

--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -22,18 +22,71 @@ const Card: React.FC = () => {
           </div>
         </section>
 
-        <section className='card-section3'>
-          <h2 className='card-title2'>O ALEITAMENTO MATERNO COSTUMA SER CLASSIFICADO EM SEIS TIPOS</h2>
-          <div>
-            <p className='card-p'><b>• Aleitamento materno exclusivo:</b> a criança recebe apenas leite materno, seja direto da mama, ordenhado ou de outra fonte, sem consumir outros líquidos ou sólidos, exceto por gotas ou xaropes com vitaminas, sais de reidratação, suplementos minerais ou medicamentos.</p>
-          </div>
-          <div>
-            <p className='card-p'><b>• Aleitamento materno predominante:</b> além do leite materno, a criança também recebe água, bebidas à base de água (como chás, infusões, água adocicada), sucos de frutas ou líquidos rituais.</p>
-          </div>
-          <div>
-            <p className='card-p'><b>• Aleitamento materno:</b> a criança é alimentada com leite materno, direto da mama ou ordenhado, podendo ou não receber outros alimentos.</p>
-          </div>
+        <section className='card-section2'>
+          <h2 className='card-title2'>QUAIS SÃO OS PRINCIPAIS TIPOS DE ALEITAMENTO MATERNO?</h2>
+          <ul className='card-list'>
+            <li>
+              <b>Aleitamento materno exclusivo:</b> a criança recebe apenas leite materno, seja direto da mama, ordenhado ou de outra fonte, sem consumir outros líquidos ou sólidos, exceto por gotas ou xaropes com vitaminas, sais de reidratação, suplementos minerais ou medicamentos.
+            </li>
+            <li>
+              <b>Aleitamento materno predominante:</b> além do leite materno, a criança também recebe água, bebidas à base de água (como chás, infusões, água adocicada), sucos de frutas ou líquidos rituais.
+            </li>
+            <li>
+              <b>Aleitamento materno:</b> a criança é alimentada com leite materno, direto da mama ou ordenhado, podendo ou não receber outros alimentos.
+            </li>
+            <li>
+              <b>Aleitamento materno complementado:</b> a criança recebe leite materno junto com alimentos sólidos ou semissólidos, que complementam, mas não substituem o leite materno.
+            </li>
+            <li>
+              <b>Aleitamento materno misto ou parcial:</b> a criança é alimentada tanto com leite materno quanto com outros tipos de leite.
+            </li>
+            <li>
+              No segundo ano de vida, o leite materno continua sendo uma fonte importante de nutrientes, fornecendo grande parte das necessidades de vitaminas, proteínas e energia. Além disso, ele mantém a proteção contra infecções, e estudos mostram que crianças não amamentadas nessa fase têm quase o dobro de risco de morrer por doenças infecciosas (WORLD HEALTH ORGANIZATION, 2000).
+            </li>
+          </ul>
         </section>
+
+
+        <section className='card-section2'>
+          <h2 className='card-title2'>BENEFÍCIOS DO ALEITAMENTO MATERNO</h2>
+          <div>
+            <p className='card-p'>No AM há benefícios fundamentais para a saúde do recém nascido e também para o corpo da mãe enquanto amamenta, sendo de curto e longo prazo para ambos.</p>
+          </div>
+          <h2 className='card-title3'><b>Para o Recém Nascido</b></h2>
+          <ul className='card-list'>
+            <li>
+              Diminui risco de alergias, hipertensão, colesterol alto, obesidade, diabetes, diarreia, infecções respiratórias e mortalidade infantil
+            </li>
+          </ul>
+          <p className='card-p'>
+            A amamentação tem efeitos protetores importantes na saúde da criança, ajudando a prevenir diversas doenças, como alergias, doenças cardiovasculares, diabetes, e infecções, além de reduzir a mortalidade infantil. Isso ocorre devido aos componentes imunológicos presentes no leite materno, que fortalecem o sistema imunológico da criança.(Ministério da Saúde, 2022)
+          </p>
+          <ul className='card-list'>
+            <li>
+              Contém nutrientes essenciais para um melhor crescimento e desenvolvimento da criança.
+            </li>
+          </ul>
+          <p className='card-p'>
+            Leite materno possui todos os nutrientes necessários, como proteínas, gorduras, vitaminas e minerais, na proporção ideal para o desenvolvimento físico e cognitivo saudável do bebê, contribuindo para seu crescimento de maneira equilibrada. (Ministério da Saúde, 2019)
+          </p>
+          <ul className='card-list'>
+            <li>
+              Promove melhor desenvolvimento da cavidade bucal, preparando para a mastigação e fala.
+            </li>
+          </ul>
+          <p className='card-p'>
+            A sucção durante a amamentação estimula o desenvolvimento adequado dos músculos faciais, ajudando na formação da mandíbula e no alinhamento correto dos dentes. Isso também facilita a transição para a mastigação de alimentos sólidos e contribui para o desenvolvimento da fala.
+          </p>
+          <ul className='card-list'>
+            <li>
+              Proporciona a formação de crianças, adolescentes e adultos mais saudáveis.
+            </li>
+          </ul>
+          <p className='card-p'>
+            O aleitamento materno estabelece uma base sólida de saúde para toda a vida. Crianças que foram amamentadas tendem a apresentar menos problemas de saúde ao longo do crescimento, sendo menos propensas à obesidade, hipertensão e outras doenças crônicas na adolescência e na fase adulta. (Ministério da Saúde, 2022)
+          </p>
+        </section>
+
       </div>
     </>
   );
